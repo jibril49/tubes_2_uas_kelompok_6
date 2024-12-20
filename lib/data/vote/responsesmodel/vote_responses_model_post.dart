352e090b9c84a3f8ec74abf64554e73dd219fbee
keisyanazwa <keisyanazwa2004@gmail.com>
2024-12-20 22:11:57 +0700

    feat : config structure project mvvm & add lib dio & get

---
+++ lib/data/vote/responsesmodel/vote_responses_model_post.dart
@@ -0,0 +1,8 @@
+// To parse this JSON data, do
+//
+// final voteResponsesModelPost = voteResponsesModelPostFromJson(jsonString);
+
+import 'package:meta/meta.dart'
+import 'dart:convert';
+
+VoteResponsesModelPost voteResponsesModelPostFromJson(String str) => Vote 
